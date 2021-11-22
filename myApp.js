ae61ba93afbaee724bdbe4917fe57e0de6ce0d6e
Yarrison allain <uwenayoallain@gmail.com>
2021-11-22 11:30:47 +0200

    uppercase from .env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require("path");
 var express = require("express");
 
@@ -8,6 +9,10 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views/index.html"))
 );
-app.get("/json", (req, res) => res.json({ message: string }));
-console.log(string);
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE == "uppercase" ? string.toUpperCase() : string
+  });
+});
 module.exports = app;
