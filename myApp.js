374c67446db739a399903732bbbb43546fc55e7a
Yarrison allain <uwenayoallain@gmail.com>
2021-11-22 10:32:35 +0200

    sending json

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 const path = require("path");
 var express = require("express");
+
 var app = express();
-const string = "Hello Express";
+const string = "Hello json";
 app.use("/public", express.static(path.join(__dirname, "public")));
-function handle(req, res) {
-  return res.sendFile(path.join(__dirname, "views/index.html"));
-}
-app.get("/", handle);
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views/index.html"))
+);
+app.get("/json", (req, res) => res.json({ message: string }));
 console.log(string);
 module.exports = app;
