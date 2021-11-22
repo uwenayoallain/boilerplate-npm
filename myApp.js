077956dc4a2115d1dc5b18675d0fa9d87e673929
Yarrison allain <uwenayoallain@gmail.com>
2021-11-22 10:20:17 +0200

    adding css to index.html

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ const path = require("path");
 var express = require("express");
 var app = express();
 const string = "Hello Express";
+app.use("/public", express.static(path.join(__dirname, "public")));
 function handle(req, res) {
   return res.sendFile(path.join(__dirname, "views/index.html"));
 }
