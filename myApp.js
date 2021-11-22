c4e3a86381b0e74cc1f1d41a5f2dd7babb78c670
Yarrison allain <uwenayoallain@gmail.com>
2021-11-22 15:16:33 +0200

    Getting form data

--- myApp.js
+++ myApp.js
@@ -11,26 +11,49 @@ const middleware = (req, res, next) => {
   next();
 };
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
+
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views/index.html"))
 );
+
 app.route("/now").get(middleware, (req, res) => {
   res.send({
     time: req.time,
   });
 });
+
 app.get("/name", (req, res) => {
   const { first, last } = req.query;
+  if ((first || last) === undefined) {
+    return res.status(400).json({
+      Error: "Please enter both first and last name",
+    });
+  }
   res.json({ name: first + " " + last });
 });
+
+app.post("/name", (req, res) => {
+  const { first, last } = req.body;
+  if ((first || last) === undefined) {
+    return res.status(400).json({
+      Error: "Please enter both first and last name",
+    });
+  }
+  res.json({ name: first + " " + last }).status(200);
+});
+
 app.get("/:word/echo", (req, res) => {
   const word = req.params.word;
   res.json({ echo: word });
 });
+
 app.get("/json", (req, res) => {
   if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({ message: string.toUpperCase() });
@@ -42,4 +65,5 @@ app.get("/json", (req, res) => {
   //       process.env.MESSAGE_STYLE == "uppercase" ? string.toUpperCase() : string
   //   });
 });
+
 module.exports = app;
